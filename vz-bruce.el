7fc48ceee0ab037d19ac201876b90385adb43616
Dr. Volker Zell <vzell@volkerzell.de>
2023-06-22 13:41:20 +0200

    germany

--- vz-bruce.el
+++ vz-bruce.el
@@ -4122,6 +4122,7 @@ Do this ALWAYS, except for the above exceptions."
 		   ("6658f787-692d-417f-852c-dcca728d5849" . "Edinburgh")
 		   ("89a675c2-3e37-3518-b83c-418bad59a85a" . "Europe")
 		   ("42f86940-0f68-4ce0-8876-d070d424d91c" . "Freehold")
+		   ("85752fda-13c4-31a3-bee5-0e5cb1f51dad" . "Germany")
 		   ("ef1b7cc0-cd26-36f4-8ea0-04d9623786c7" . "Netherlands")
 		   ("ef1b7cc0-cd26-36f4-8ea0-04d9623786c7" . "The Netherlands")
 		   ("85c7cd5f-6fe2-4195-a44d-69fa390bd6ec" . "Newark")
